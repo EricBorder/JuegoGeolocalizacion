cede12e4bca43f4bfd694feed8289847f4b48f01
EricBorder <magicsergy@gmail.com>
2023-02-27 10:40:40 +0100

    Cambios

--- app/src/main/java/com/example/juegogeolocalizacion/ui/main/CameraFragment.kt
+++ app/src/main/java/com/example/juegogeolocalizacion/ui/main/CameraFragment.kt
@@ -1,65 +1,60 @@
 package com.example.juegogeolocalizacion.ui.main
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.juegogeolocalizacion.R
 
-class CameraFragment : Fragment() {
-    companion object {
-        fun newInstance() = CameraFragment()
-    }
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-    private lateinit var viewModel: MainViewModel
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CameraFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CameraFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-        //Instanciamos ViewModel que usaremos para llamar a nuestra clase ViewModel
-        /* val miModelo by viewModels<MainViewModel>()
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
-        //Observamos el cambio en el random
-        miModelo.liveRandom.observe(
-            this,
-            Observer(
-                fun(random: Int) {
-                    var tvRandom: TextView? = view?.findViewById(R.id.sumaRandom1)
-                    tvRandom?.setText("$random")
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
 
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CameraFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CameraFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
-            )
-
-        )
-        //Observamos el cambio en el random2
-        miModelo.liveRandom2.observe(
-            this,
-            Observer(
-                fun(random2: Int) {
-                    var tvRandom2: TextView? = view?.findViewById(R.id.sumaRandom2)
-                    tvRandom2?.setText("$random2")
-
-                }
-            )
-
-        )
-        //Observamos el cambio en el resultado
-        miModelo.liveResultado.observe(
-            this,
-            Observer(
-                fun(resultado: Int) {
-                    var tvResultado: TextView? = view?.findViewById(R.id.sumaResultado)
-                    tvResultado?.setText("$resultado")
-
-                }
-            )
-        )*/
-
+            }
     }
 }
-
